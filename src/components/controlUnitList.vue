01c034562aedd349a4d79991a2e5f8c0a3d797e9
adrianjost <22987140+adrianjost@users.noreply.github.com>
2019-11-17 16:57:12 +0100

    improve icon readability

--- src/components/controlUnitList.vue
+++ src/components/controlUnitList.vue
@@ -7,14 +7,18 @@
 			:to="'/settings/edit/' + controlUnit.id"
 			:style="{ background: controlUnit.background }"
 		>
-			<i class="material-icons">
-				{{ controlUnit.icon }}
-			</i>
+			<div class="icon-container">
+				<i class="material-icons">
+					{{ controlUnit.icon }}
+				</i>
+			</div>
 		</RouterLink>
 		<RouterLink class="unit add" :to="addUrl">
-			<i class="material-icons">
-				add
-			</i>
+			<div class="icon-container">
+				<i class="material-icons">
+					add
+				</i>
+			</div>
 		</RouterLink>
 	</div>
 </template>
@@ -35,18 +39,50 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$iconSize: 24px;
+$iconPadding: 16px;
 .unit {
+	position: relative;
 	display: inline-block;
-	padding: 16px;
+	width: $iconSize + (2 * $iconPadding) + 2px;
+	height: $iconSize + (2 * $iconPadding) + 2px;
+	padding: $iconPadding;
 	margin: 8px;
 	font-size: 0;
-	color: var(--color-text);
 	border: 1px solid var(--color-border);
 	border-radius: 50%;
+
+	.icon-container {
+		position: absolute;
+		top: 0;
+		left: 0;
+		padding: $iconPadding;
+		color: var(--color-text);
+	}
+
+	&:before {
+		$spaceAround: 8px;
+		position: absolute;
+		top: $spaceAround;
+		left: $spaceAround;
+		width: calc(100% - 2 * #{$spaceAround});
+		height: calc(100% - 2 * #{$spaceAround});
+		content: "";
+		background: var(--color-overlay);
+		border-radius: 100%;
+	}
 
 	&.add {
 		color: var(--color-border);
 		border-width: 2px;
+		.icon-container {
+			// stylelint-disable plugin/8-point-grid
+			// adjust offset because the border is 1px larger
+			// this moves the 0 position by 1 px;
+			top: -1px;
+			left: -1px;
+			// stylelint-enable plugin/8-point-grid
+		}
 	}
 }
 </style>
