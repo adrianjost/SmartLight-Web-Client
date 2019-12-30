fceaf3e7d0dc36efc0d382607fb7cfba7114dbbc
adrianjost <22987140+adrianjost@users.noreply.github.com>
2019-12-30 13:42:23 +0100

    add sentry integration

--- src/main.js
+++ src/main.js
@@ -1,12 +1,26 @@
 import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
 import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
 
 Vue.prototype.$eventHub = new Vue(); // Global event bus
+
+import pkg from "../package.json";
+import * as Sentry from "@sentry/browser";
+import * as Integrations from "@sentry/integrations";
+if (process.env.VUE_APP_SENTRY_DSN) {
+	Sentry.init({
+		dsn: process.env.VUE_APP_SENTRY_DSN,
+		environment: process.env.NODE_ENV,
+		release: pkg.version,
+		integrations: [
+			new Integrations.Vue({ Vue, attachProps: true, logErrors: true }),
+			new Integrations.CaptureConsole({
+				levels: ["warn", "error", "debug", "assert"],
+			}),
+		],
+	});
+}
 
 import Toasted from "vue-toasted";
 Vue.use(Toasted, {
@@ -24,6 +38,9 @@ Vue.directive("ripple", Ripple);
 import toast from "@/mixins/toast.js";
 Vue.mixin(toast);
 
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 export default new Vue({
 	router,
 	store,
