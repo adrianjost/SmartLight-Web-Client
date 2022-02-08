d58f2034bb98f68d58f2f3d580f29cd252c3b75a
TODO <todo-actions[bot]@users.noreply.github.com>
2022-02-08 16:06:27 +0000

    Collect TODO comments

--- src/helpers/firebase.js
+++ src/helpers/firebase.js
@@ -9,7 +9,7 @@ import { config } from "./firebaseConfig";
 export const firebaseApp = initializeApp(config);
 const db = getFirestore(firebaseApp);
 
-// TODO: enable performance monitoring
+// TODO [$620295039df3610009f66d5a]: enable performance monitoring
 // import { getPerformance } from "firebase/performance";
 // const perf = getPerformance(app);
 
